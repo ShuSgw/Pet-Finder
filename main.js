831a233b036371a1297722e917bdae7ed0c011af
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2018-06-03 19:55:53 -0700

    completed

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
+window.onload = ()=>{
+  var containers = document.getElementsByClassName("wrap");
+  containers = Array.from(containers);
+  containers.forEach(container=>{
+    container.classList.remove("fade");
+  });
+};
+
 var btn = document.getElementById("btn");
 var navbar = document.getElementById("navbar");
 btn.addEventListener('click', ()=>{
   var hidenav = document.getElementById("navbarText");
-  if (hidenav.classList.contains("collapse")) {
-    navbar.classList.add("bg-white");
-    hidenav.classList.remove("collapse");
-  }else{
-    navbar.classList.remove("bg-white");
-    hidenav.classList.add("collapse");
-  }
+  hidenav.classList.toggle("collapse");
+  navbar.classList.toggle("bg-white");
+  hidenav.classList.toggle("fadeNav");
+  // if (hidenav.classList.contains("collapse")) {
+  //   //表示されてる時
+  //   navbar.classList.add("bg-white");
+  //   hidenav.classList.remove("collapse");
+  // }else{
+  //   //押されてない時
+  //   navbar.classList.remove("bg-white");
+  //   hidenav.classList.add("collapse");
+  // }
 })
